47d992564fb22d3c7b1b67a1bde505328811e1e9
Errin Johnson <sporttrac64@outlook.com>
2021-06-04 22:06:53 -0400

    Create nav-head-template.js
    added file to main branch

---
+++ nav-head-template.js
@@ -0,0 +1,8 @@
+function addElements() {
+    let include1 = document.getElementById("include");
+    let newDiv = document.createElement("title");
+    let t = document.createTextNode("Errin Johnson | Software Developer");
+    newDiv.appendChild(t);
+    include1.appendChild(newDiv);
+}
+addElements();
